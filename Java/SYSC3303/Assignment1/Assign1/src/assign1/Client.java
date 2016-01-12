06a0e7a5b79d5da698b2edcea19e302eb7319153
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2016-01-12 16:14:00 -0500

    initial commit server-client udp

---
+++ Java/SYSC3303/Assignment1/Assign1/src/assign1/Client.java
@@ -0,0 +1,25 @@
+package assign1;
+
+import java.net.DatagramPacket;
+import java.net.Socket;
+
+public class Client {
+	
+	private Socket socket;
+
+	public Client(){
+		socket = new Socket();
+		
+		
+		
+	}
+	
+	private DatagramPacket readRequest(){
+		
+	}
+	
+	private DatagramPacket writeRequest(){
+		
+	}
+	
+}
---
+++ Java/SYSC3303/Assignment1/Assign1/src/assign1/Server.java
@@ -0,0 +1,13 @@
+package assign1;
+
+import java.net.Socket;
+
+public class Server {
+	
+	private Socket socket;
+
+	public Server(){
+		socket = new Socket();
+	}
+	
+}
